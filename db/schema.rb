035653d3650b5bafdcb9845c0691e9a7ee250bf9
nagaraj <raj.nagaraj1990@gmail.com>
2024-03-06 12:48:27 -0500

    Add posts entity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_203319) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_06_155210) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_203319) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
